5569da1c5c5312ea50ba7fd82232c2b0d0a0ff24
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-02 19:30:58 +0200

    roles

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { RoleDto } from './dto/role.dto';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+  constructor(private rolesService: RolesService) {}
+
+  @Post()
+  create(@Body() dto: RoleDto) {
+    return this.rolesService.createRole(dto);
+  }
+
+  @Get('/:value')
+  getRoleByName(@Param('value') value: string) {
+    return this.rolesService.getRoleByName(value);
+  }
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { RoleDto } from './dto/role.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { Role } from './role.model';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
+
+  async createRole(dto: RoleDto) {
+    return await this.roleRepository.create(dto);
+  }
+
+  async getRoleByName(value: string) {
+    return await this.roleRepository.findOne({ where: { value } });
+  }
+}
