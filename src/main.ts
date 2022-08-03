418bff432eb74acd32182ecfc6734949c3e8e44d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-03 21:39:44 +0200

    validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from "./pipes/validation.pipe";
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3001;
@@ -13,6 +14,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () =>
     console.log(`Server has been started on port ${PORT}`)
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class UserDto {
   @ApiProperty({
     example: 'user@mail.domain',
     description: 'User email'
   })
+  @IsString({ message: 'has-to-be-string' })
+  @IsEmail({}, { message: 'not-email' })
   readonly email: string;
 
   @ApiProperty({
     example: '1@qWasdf',
     description: 'User password'
   })
+  @IsString({ message: 'has-to-be-string' })
+  @Length(8, 32, { message: 'min-8-max-32' })
   readonly password: string;
 }
